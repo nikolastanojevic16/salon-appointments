9dc781e1570078ae5253a808d618d4d9ee1a7d6b
nikolastanojevic16 <48658075+nikolastanojevic16@users.noreply.github.com>
2023-05-16 17:45:55 +0200

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -4,7 +4,7 @@ PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 echo -e "\n~~~~~ MY SALON ~~~~~\n"
 echo -e "Welcome to My Salon, how can I help you?\n"
 
-GET_SERVICES_ID() {
+MAIN() {
   if [[ $1 ]]
   then
     echo -e "\n$1"
@@ -19,15 +19,14 @@ GET_SERVICES_ID() {
   done
   read SERVICE_ID_SELECTED
   case $SERVICE_ID_SELECTED in
-    [1-5]) NEXT ;;
-        *) GET_SERVICES_ID "I could not find that service. What would you like today?" ;;
+    [1-5]) SERVICE_PART ;;
+        *) MAIN "I could not find that service. What would you like today?" ;;
   esac
 }
 
-NEXT() {
+SERVICE_PART() {
   echo -e "\nWhat's your phone number?"
   read CUSTOMER_PHONE
-  # CUSTOMER_PHONE_FORMATED=$(echo $CUSTOMER_PHONE | sed 's/[^0-9]*//g')
   NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
   CUSTOMER_NAME=$(echo $NAME | sed 's/ //g')
   if [[ -z $NAME ]]
@@ -35,7 +34,7 @@ NEXT() {
     echo -e "\nI don't have a record for that phone number, what's your name?"
     read CUSTOMER_NAME
     NAME=$(echo $NAME | sed 's/ //g')
-    SAVED_TO_TABLE_CUSTOMERS=$($PSQL "INSERT INTO customers(name,phone) VALUES('$CUSTOMER_NAME','$CUSTOMER_PHONE')")
+    INSERT_CUSTOMER=$($PSQL "INSERT INTO customers(name,phone) VALUES('$CUSTOMER_NAME','$CUSTOMER_PHONE')")
   fi
   
   GET_SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
@@ -44,12 +43,8 @@ NEXT() {
   
   echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
   read SERVICE_TIME
-  SAVED_TO_TABLE_APPOINTMENTS=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
-  if [[ $SAVED_TO_TABLE_APPOINTMENTS == "INSERT 0 1" ]]
-  then
-    echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
-  fi
-
+  INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+  echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
 }
 
-GET_SERVICES_ID
+MAIN
