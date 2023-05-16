b00e7014a40ad9e8912828017b572247e463b703
nikolastanojevic16 <48658075+nikolastanojevic16@users.noreply.github.com>
2023-05-16 17:49:13 +0200

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -10,7 +10,7 @@ MAIN() {
     echo -e "\n$1"
   fi
   
-  LIST_SERVICES=$($PSQL "SELECT * FROM services")
+  LIST_SERVICES=$($PSQL "select * from services")
   echo "$LIST_SERVICES" | while read SERVICE_ID BAR SERVICE
   do
     ID=$(echo $SERVICE_ID | sed 's/ //g')
@@ -27,23 +27,21 @@ MAIN() {
 SERVICE_PART() {
   echo -e "\nWhat's your phone number?"
   read CUSTOMER_PHONE
-  NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+  NAME=$($PSQL "select name from customers where phone='$CUSTOMER_PHONE'")
   CUSTOMER_NAME=$(echo $NAME | sed 's/ //g')
   if [[ -z $NAME ]]
   then
     echo -e "\nI don't have a record for that phone number, what's your name?"
     read CUSTOMER_NAME
-    NAME=$(echo $NAME | sed 's/ //g')
-    INSERT_CUSTOMER=$($PSQL "INSERT INTO customers(name,phone) VALUES('$CUSTOMER_NAME','$CUSTOMER_PHONE')")
+    INSERT_CUSTOMER=$($PSQL "insert into customers(name,phone) VALUES('$CUSTOMER_NAME','$CUSTOMER_PHONE')")
   fi
   
-  GET_SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
-  SERVICE_NAME=$(echo $GET_SERVICE_NAME| sed 's/ //g')
-  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+  GET_SERVICE_NAME=$($PSQL "select name from services where service_id=$SERVICE_ID_SELECTED")
+  CUSTOMER_ID=$($PSQL "select customer_id from customers where phone='$CUSTOMER_PHONE'")
   
   echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
   read SERVICE_TIME
-  INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+  INSERT_APPOINTMENT=$($PSQL "insert into appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
   echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
 }
 
